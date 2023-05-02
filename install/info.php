ab26ec6983403394e1d55536ff5138b52dd9703c
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-05-02 08:47:45 -0700

    cleanup

--- install/info.php
+++
@@ -1,7 +0,0 @@
-<?php namespace ProcessWire;
-if(!defined("PROCESSWIRE_INSTALL")) die();
-$info = array(
-	'title' => "Rockfrontend Empty", 
-	'summary' => "Install into a directory called site-rockfrontend-empty in a new ProcessWire installation.", 
-	'screenshot' => "tumblr_mnwtscshlq1qabcs8o1_1280.png"
-);
